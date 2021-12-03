faf6f01f3d134b5f4b0b4da770f19f8fccb91a45
Nicole Huang <nhuang@lcs.on.ca>
2021-12-03 11:09:08 -0500

    Initial Commit

---
+++ TemperatureConverterSwitchStatement/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemperatureConverterSwitchStatement
+//
+//  Created by Nicole on 2021/12/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TemperatureConverterSwitchStatement/TemperatureConverterSwitchStatementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterSwitchStatementApp.swift
+//  TemperatureConverterSwitchStatement
+//
+//  Created by Nicole on 2021/12/3.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterSwitchStatementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
